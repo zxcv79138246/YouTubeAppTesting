6bc2be038f388377eea619d2f3d1f1d923c3f334
Mystia <as80110680@gmail.com>
2017-05-15 17:39:10 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # YouTubeAppTesting
+
+#Appium install
+
+1. python (我用3.6)
+2. appium-python-client (cmd打 ==pip install Appium-Python-Client==)
+3. 安裝python unittest (cmd打 ==pip install pytest==)
+4. node.js
+5. appium (cmd打 ==npm install appium==)
+6. appium-doctor (cmd打 ==npm install appium-doctor==)
+7. 安裝java sdk (有裝過就跳過)
+8. Android Studio (裝Android SDK和AVD Manager，android studio的比較好用)
+
+- 在cmd打==appium-doctor==後，這裡會提醒那些東西有缺，改掉打x的地方就好了，上面東西都裝完的話就只需要修改環境變數
+
+- IDE我用visual studio，在Python Environment新增一個，然後把Prefix path、Interpreter path、Windows interpreter和Library path填上，預設有提示
+
+- 都裝好以後，cmd打==appium==就可以啟動appium伺服器，從android studio打開模擬器後，appium會抓到adb上的devices，就可以直接寫腳本做測試了
---
+++ YouTubeAppTesting/YouTubeAppTesting/YouTubeAppTesting.py
@@ -0,0 +1,151 @@
+import unittest
+
+from appium.webdriver.common.touch_action import TouchAction
+from appium import webdriver
+from time import sleep
+
+
+class TestClass(unittest.TestCase):
+
+    def setUp(self):
+        desired_caps = {}
+        desired_caps['platformName'] = 'Android'
+        desired_caps['platformVersion'] = '7.1.1'
+        desired_caps['deviceName'] = 'Android Emulator'
+        desired_caps['app'] = R'C:\Users\Mystia\documents\visual studio 2015\Projects\test\test\YouTube_12.17.apk'
+        desired_caps['appPackage'] =  'com.google.android.youtube'
+        desired_caps['appActivity'] = 'com.google.android.youtube.HomeActivity'
+        #desired_caps['appActivity'] = 'com.google.android.apps.youtube.app.application.Shell$HomeActivity'
+
+        print('opening youtube ...')
+        self.dr = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+    def test0(self):
+        print("wait for opening")
+        sleep(2)
+
+        print('click menu button ...')
+        self.dr.find_element_by_accessibility_id("更多選項").click()
+        sleep(1)
+
+        print('click login button ...')
+        try :
+            t = self.dr.find_element_by_android_uiautomator('new UiSelector().text("登入")').click()
+            sleep(1)
+            self.dr.find_element_by_android_uiautomator('new UiSelector().text("軟測")').click()
+        except:
+            print("already login")
+            self.dr.tap([(555,1629)] ,100)
+        sleep(0.5)
+        
+        self.dr.tap([(555,1629)] ,100)
+        sleep(2)
+
+        print('click subscribe ...')
+        self.dr.find_element_by_accessibility_id("訂閱這個頻道").click()
+        sleep(1)    
+
+        print('click add button ...')
+        self.dr.find_element_by_android_uiautomator('new UiSelector().text("新增至")').click()
+        sleep(1)      
+
+        print('click watch later button ...')
+        self.dr.find_element_by_android_uiautomator('new UiSelector().text("稍後觀看")').click()
+        sleep(1)      
+
+        print('back ...')
+        self.dr.press_keycode(4)
+        sleep(2)
+
+        print('slide left ...')
+        self.dr.swipe(930, 1500, 100, 150, 300)
+        sleep(2)
+
+        print('click account ...')
+        self.dr.find_element_by_accessibility_id("帳戶").click()
+        sleep(1)   
+
+        print('click watch later button ...')
+        self.dr.find_element_by_android_uiautomator('new UiSelector().text("稍後觀看")').click()
+        sleep(1)    
+
+        print('click menu ...')
+        self.dr.find_element_by_accessibility_id("選單").click()
+        sleep(1)   
+
+        print('remove from watch late ...')
+        self.dr.find_element_by_android_uiautomator('new UiSelector().text("從稍後觀看中移除")').click()
+        sleep(1)          
+
+        print('back ...')
+        self.dr.press_keycode(4)
+        sleep(2)
+
+        print('click subscribe ...')
+        self.dr.find_element_by_accessibility_id("訂閱內容").click()
+        sleep(1)   
+
+        print('menu ...')
+        self.dr.find_element_by_accessibility_id("選單").click()
+        sleep(1)   
+
+        print('canel subscribe ...')
+        self.dr.find_element_by_android_uiautomator('new UiSelector().text("取消訂閱")').click()
+        sleep(1)   
+
+        print('click menu button ...')
+        self.dr.find_element_by_accessibility_id("更多選項").click()
+        sleep(1)
+
+        print('click logout button ...')
+        self.dr.find_element_by_android_uiautomator('new UiSelector().text("登出")').click()
+        sleep(1)
+
+    def test1(self):
+        print("wait for opening")
+        sleep(2)
+
+        print('click account ...')
+        self.dr.find_element_by_accessibility_id("帳戶").click()
+        sleep(2)   
+
+        print('click recommend video ...')
+        self.dr.find_element_by_accessibility_id("發燒影片").click()
+        sleep(2)   
+
+        print('click a random video ...')
+        self.dr.tap([(530,1000)] ,100)
+        sleep(3)   
+
+        print('click video')
+        self.dr.tap([(500,350)] ,100)
+        sleep(0.5)    
+
+        print('click option')
+        self.dr.find_element_by_accessibility_id("更多選項").click()
+        sleep(1)                 
+
+        print('click quality')
+        self.dr.find_element_by_android_uiautomator('new UiSelector().text("畫質")').click()
+        sleep(1)          
+
+        try :
+            t = self.dr.find_element_by_android_uiautomator('new UiSelector().text("360p")').click()
+            sleep(1)
+        except:
+            print('doesn\'t support 360p video')
+
+        print('click video')
+        self.dr.tap([(500,350)] ,100)
+        sleep(0.5)
+
+        print('pause video')
+        self.dr.find_element_by_accessibility_id("暫停影片").click()
+        sleep(5)
+
+
+    def tearDown(self):
+        self.dr.quit()
+
+if __name__ == '__main__':
+    unittest.main()
